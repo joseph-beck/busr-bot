9ceaba17763f8b22840c2098c52998ac4b240ee8
joseph <joebeck03@icloud.com>
2023-04-05 23:56:49 +0000

    old lib stuff

---
+++ pkg/sql/conn.go
@@ -0,0 +1,38 @@
+package database
+
+import (
+	"sync"
+
+	"github.com/jmoiron/sqlx"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var lock = &sync.Mutex{}
+
+type Conn struct {
+	db sqlx.DB
+}
+
+var instance *Conn
+
+func Connect() *Conn {
+	if instance == nil {
+		lock.Lock()
+		defer lock.Unlock()
+
+		instance = &Conn{connDb()}
+	}
+
+	return instance
+}
+
+func connDb() sqlx.DB {
+	db, err := sqlx.Open("mysql", connStr())
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	return *db
+}
---
+++ pkg/sql/conn_str.go
@@ -0,0 +1,36 @@
+package database
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+type connData struct {
+	Username string
+	Password string
+	Address  string
+	Port     string
+	Database string
+}
+
+func connStr() string {
+	content, err := ioutil.ReadFile("../../configs/database.json")
+	if err != nil {
+		fmt.Println("Error opening file: ", err)
+	}
+
+	cd := connData{}
+	err = json.Unmarshal(content, &cd)
+	if err != nil {
+		fmt.Println("Error duing unmarshall() :", err)
+	}
+
+	return fmt.Sprintf(
+		"%s:%s@(%s:%s)/%s",
+		cd.Username,
+		cd.Password,
+		cd.Address,
+		cd.Port,
+		cd.Database)
+}
