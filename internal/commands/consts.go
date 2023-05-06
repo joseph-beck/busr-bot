bead938d28cfb9a46afd93ad360f1fc629e53fd8
joseph <joebeck03@icloud.com>
2023-05-06 13:34:25 +0100

    cleanup

--- internal/commands/consts.go
+++ internal/commands/consts.go
@@ -1,8 +1,10 @@
 package commands
 
-const invalidTarget string = ">>> **Not a valid target**"
-const invalidDriver string = ">>> **Driver does not exist**"
-const invalidSeason string = ">>> **Season does not exist**"
-const invalidQuali string = ">>> **Qualifying does not exist**"
-const invalidSprint string = ">>> **Sprint does not exist**"
-const invalidRace string = ">>> **Race does not exist**"
+const (
+	invalidTarget string = ">>> **Not a valid target**"
+	invalidDriver string = ">>> **Driver does not exist**"
+	invalidSeason string = ">>> **Season does not exist**"
+	invalidQuali  string = ">>> **Qualifying does not exist**"
+	invalidSprint string = ">>> **Sprint does not exist**"
+	invalidRace   string = ">>> **Race does not exist**"
+)
